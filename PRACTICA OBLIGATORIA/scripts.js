3699f9d90dff72f4978e5e3c5819eb13214f2117
Jorge <jomedi01@ucm.es>
2022-12-11 11:44:13 +0100

    ejs sin jQuery

--- PRACTICA OBLIGATORIA/scripts.js
+++
@@ -1,9 +0,0 @@
-$('#picker').on('change', function() {
-    $('#form2 #' + $(this).val()).prop('disabled', false).siblings().prop('disabled', true);
-});
-
-
-
-// $('#inputState').on('change', function() {
-//     $($(this).val()).prop('disabled', false);
-// });
